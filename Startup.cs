8fda1beea7bcac9e1cd23c2424263ca2d71eca73
Osama Abdel Latif Diab <o.diab@outlook.com>
2020-05-17 13:27:02 +0200

    Initial Project Structure

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SuperPagination
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder application, IWebHostEnvironment environment)
+        {
+        }
+    }
+}
